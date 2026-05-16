d0eef70f87a7271390f64398d7e9ea1b12857250
Jefferson Matheus <matheus.jefferson@escolar.ifrn.edu.br>
2026-05-16 12:34:51 -0300

    :tada: init: Estrutura inicial

---
+++ backend/gestaoEscolar/Model/Aluno.cs
@@ -0,0 +1,11 @@
+﻿namespace gestaoEscolar;
+
+public class Aluno
+{
+    private int Id {get; set;}
+    private string Nome {get; set;}
+    private string Email {get; set;}
+
+    private String Curso {get; set;}
+    private DateTime DataNascimento {get; set;}
+}
